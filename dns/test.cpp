368f6ed8e4714dc15b30438c9c694e456b82e27f
chenwen <1287577782@qq.com>
2020-04-28 00:31:50 +0800

    add dns protocol

--- dns/test.cpp
+++ dns/test.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#define BUF_SIZE 1024
+#define SER_PORT 53
 int main(int argc ,char **argv)
 {
+    unsigned char buf[BUF_SIZE];
+    int serverfd,clientfd,len=0,i;
+    struct sockaddr_in serveraddr,addr;
+
     return 0;
 }
